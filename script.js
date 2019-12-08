d9e3e15becf029321625e657eac0d36d55f949ef
uber_vlad <idroidservv@gmail.com>
2019-12-08 19:41:41 +0300

    + Button = Autoloading is toggled off as default

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
 // ==UserScript==
-// @name         Umschool auto video loader
+// @name         Umschool script
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  test script
-// @author       vk.com/uber_vlad
+// @version      0.2.1 beta
+// @description  Скрипт для того, чтобы запускать вебинары на ютубе
+// @author       https://vk.com/uber_vlad
 // @match        https://new.umschool.net/mastergroup/lessons/*
 // @grant        none
 // ==/UserScript==
 
-/*var elem = document.body.getElementsByClassName('preview-title')[0];
-if (elem)
-{
-    elem.innerHTML += '<button onclick="alert(num)">YT</button>';
-}*/
-var clock = setTimeout(yt_start, 1500);
+var style='#yt_button_script {background-color: #be2413; border: none; border-radius: 4px; color: white; padding: 10px 21px; text-align: center; text-decoration: none; display: inline-block; font-size: 18px; font-weight: bold;} #yt_button_script:hover {background-color: #df3c2a;} #yt_button_script:active {background-color: #d96154;}';
+var add_text = '<b style="margin-left: 15px;">Скрипт в бете и если что-то не так, то пишите <a href="https://vk.com/uber_vlad" target="_blank">сюда</a>.</b> <br><br><block style="color: grey;">Для включения автозапуска поищите в скрипте пункт <i>autoloading</i></block>';
 
+var autoloading = false; // переключатель автозапуска
+var src;
+var clock;
+var timer = 1.5; // таймер для автозапуска
+var internal_timer = 0.5; // таймер для поиска и добавления элементов
+var button;
+
+clock = setTimeout(start, internal_timer * 1000);
+
+function start()
+{
+    var elem = document.body.getElementsByClassName('content')[0];
+    if (elem)
+    {
+        elem.innerHTML += '<br><input id="yt_button_script" type="button" value="Открыть на ютубе"> ' + add_text;
+        document.head.innerHTML += '<style>' + style + '</style>';
+        button = document.getElementById('yt_button_script');
+    }
+    else
+    {
+        alert('Скрипт не нашёл места чтобы поместить ссылку автозапуска, поэтому всё откроется автоматически через ' + timer + ' секунд(ы)');
+    }
+
+    if (autoloading)
+    {
+        clock = setTimeout(yt_start, timer * 1000);
+    }
+}
+
+button.onclick = function button_click() { yt_start(); }
 
 function yt_start()
 {
-    var src = document.body.getElementsByClassName("plyr__video-wrapper")[0].children[0].src;
+    src = document.body.getElementsByClassName('plyr__video-wrapper')[0].children[0].src;
     src = 'https://youtu.be/' + src.split('embed')[1].split('?')[0];
     open(src);
 }
